ebcd2940c6361342dc4313098d7a106bdc245361
Daniel Pimentel Cardoso <102395676+DanielPimC@users.noreply.github.com>
2024-08-21 13:21:41 -0300

    Add files via upload

---
+++ src/App.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <PrimeiroComponente />
+    <LifeCycle />
+    <Pessoa />
+  </div>
+</template>
+
+<script>
+  import LifeCycle from './components/LifeCycle.vue'
+  import Pessoa from './components/Pessoa.vue'
+  import PrimeiroComponente from './components/PrimeiroComponente.vue'
+
+  export default {
+    name: 'App',
+    components: {
+      PrimeiroComponente,
+      LifeCycle,
+      Pessoa
+    }
+  }
+</script>
---
+++ src/components/PrimeiroComponente.vue
@@ -0,0 +1,18 @@
+<template>
+    <div>
+        <h1>Olá, Vue!</h1>
+        <p>Meu nome é {{ nome }}, e tenho {{ idade }} anos.</p>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: 'PrimeiroComponente',
+        data() {
+            return {
+                nome: "Daniel",
+                idade: 20
+            }
+        }
+    }
+</script>
